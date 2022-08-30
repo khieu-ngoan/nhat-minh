e5681b19b1c9ceeb58fe9b2d472f41d46b75ca30
quannh <quannh@runsystem.net>
2022-08-30 18:24:34 +0700

    update

---
+++ docs/build/precache-manifest.aa40ef5eb37ababd05e3d37a4902cb55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8487a66ad50fdbb4f62c",
+    "url": "/static/css/main.50b166eb.chunk.css"
+  },
+  {
+    "revision": "8487a66ad50fdbb4f62c",
+    "url": "/static/js/main.a662f764.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "308d762c54c1bacb311a",
+    "url": "/static/js/2.dab63d73.chunk.js"
+  },
+  {
+    "revision": "cfdb78e2379018a94909b4b012a182c2",
+    "url": "/index.html"
+  }
+];
