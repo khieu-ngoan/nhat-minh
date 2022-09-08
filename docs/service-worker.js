e37d85f2f52d16f154ffa3b30cdef1ccce1b40b0
quannh <quannh@runsystem.net>
2022-09-08 15:29:33 +0700

    update

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -15,10 +15,14 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 
 importScripts(
 <<<<<<< HEAD
+<<<<<<< HEAD
   "//quanict.github.io/nhat-minh/precache-manifest.91791a04bc779b41ff2c8c850c6c66e7.js"
 =======
   "/precache-manifest.440f1bfb655c892337267220a4340c37.js"
 >>>>>>> bb63fed (update)
+=======
+  "//quanict.github.io/nhat-minh/precache-manifest.dc2a6ede9f7f9125cab694a772095fcf.js"
+>>>>>>> 5b58c0d (update)
 );
 
 workbox.clientsClaim();
@@ -33,10 +37,14 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 workbox.routing.registerNavigationRoute("//quanict.github.io/nhat-minh/index.html", {
 =======
 workbox.routing.registerNavigationRoute("/index.html", {
 >>>>>>> bb63fed (update)
+=======
+workbox.routing.registerNavigationRoute("//quanict.github.io/nhat-minh/index.html", {
+>>>>>>> 5b58c0d (update)
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
