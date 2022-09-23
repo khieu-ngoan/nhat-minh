6045aa3fcb83e0e658bebbb92eeaa1173658a676
quannh <quannh@runsystem.net>
2022-09-23 14:22:38 +0700

    rebuild page

---
+++ build/precache-manifest.5125acc1179f0d4b37722659b9958068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62e7b2c085e4273fa8a8",
+    "url": "/nhat-minh/static/css/main.50b166eb.chunk.css"
+  },
+  {
+    "revision": "62e7b2c085e4273fa8a8",
+    "url": "/nhat-minh/static/js/main.808759ae.chunk.js"
+  },
+  {
+    "revision": "28bb7df010126fdd12f7",
+    "url": "/nhat-minh/static/js/runtime~main.a9a63b58.js"
+  },
+  {
+    "revision": "890c8c86e60169dee462",
+    "url": "/nhat-minh/static/js/2.b6dca228.chunk.js"
+  },
+  {
+    "revision": "81825d1226e6bc788e0d435ec7463df8",
+    "url": "/nhat-minh/index.html"
+  }
+];
